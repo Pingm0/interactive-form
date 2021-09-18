332f4767dab3a10c5030aad72610bfd44918c0a5
Mohammed <mohd.ntw@gmail.com>
2021-09-18 17:50:01 -0600

    pushing code to github

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ const jobRoleSelect = document.querySelector("#title");
 const otherJobRole = document.querySelector("#other-job-role");
 const design = document.querySelector("#design");
 const colorDropdown = document.querySelector("#color");
+const totalPlaceholder = document.querySelector(".activities-cost")
+let amount = 0;
 
-const colorOpstion = colorDropdown.children
-console.log(colorOpstion)
+const colorOption = colorDropdown.children
+const activities = document.querySelector('#activities-box')
+console.log(activities)
 
 
 
@@ -16,7 +19,7 @@ console.log(colorOpstion)
 nameInput.focus();
 
 // making the OtherJobRole Input hidden by defualt 
-otherJobRole.style.display = "none";
+otherJobRole.hidden = true;
 //disableing the color dropdown menue 
 colorDropdown.setAttribute("disabled","");
 
@@ -27,9 +30,11 @@ colorDropdown.setAttribute("disabled","");
 jobRoleSelect.addEventListener('change', (e) => {
     if (e.target.value === "other"){
 
-    otherJobRole.style.display = "block";
+      otherJobRole.hidden = false ;
+
 } else {
-    otherJobRole.style.display = "none";
+
+    otherJobRole.hidden = true ;
 }
 });
 
@@ -39,23 +44,88 @@ design.addEventListener('change', (e) => {
     colorDropdown.removeAttribute("disabled","");
     
     const selection = e.target.value;
-    //colorOpstion[0].style.display = "none";
-   
-   
-   for (let i = 0; i < colorOpstion.length; i++) {
-       if (colorOpstion[i].dataset.theme !== selection) {
-            //colorOpstion[i].style.display = "none";
-            colorOpstion[i].setAttribute('hidden', 'false');
+     
+   for (let i = colorOption.length -1 ; i > 0 ; i--) {
+       if (colorOption[i].dataset.theme === selection) {
+        colorOption[i].hidden = false;
+        colorOption[i].setAttribute('selected', true)
+       
        }
        else {
 
-        //colorOpstion[i].style.display = "block";
-        colorOpstion[i].setAttribute('selected', 'true');
-
-       }       
-   }
-   //colorOpstion[0].setAttribute('selected', 'true');
+        colorOption[i].hidden = true;
+        colorOption[i].removeAttribute('selected', false)
+       }      
+   }  
 });
+
+
+// Activities section work 
+
+activities.addEventListener('click', (e) => {
+
+    console.log(e.target)
+
+    if (e.target.tagName === "INPUT") {
+
+        if (e.target.checked) {
+            amount += parseInt(e.target.dataset.cost)
+        }
+        else {
+            amount -= parseInt(e.target.dataset.cost)
+        }   
+    }
+    totalPlaceholder.innerHTML = `Total: $${amount}`
+})
+
+
+
+// Credicat card Section
+const creditCard = document.querySelector("#credit-card")
+const paypal = document.querySelector("#paypal")
+const bitcoin = document.querySelector("#bitcoin")
+const paymentSelector = document.querySelector("#payment")
+
+paymentSelector.firstElementChild.nextElementSibling.setAttribute("selected", true)
+
+paypal.hidden = true;
+bitcoin.hidden = true;
+
+paymentSelector.addEventListener("change", (e) => {
+
+    const showPaySection = e.target.value ;
+
+    if (showPaySection == "credit-card") {
+        creditCard.hidden = false;
+        paypal.hidden = true;
+        bitcoin.hidden = true;
+    }
+    else if (showPaySection == "paypal") {
+        creditCard.hidden = true;
+        paypal.hidden = false;
+        bitcoin.hidden = true;
+    }
+
+    else {
+        
+        creditCard.hidden = true;
+        paypal.hidden = true;
+        bitcoin.hidden = false;
+
+    }
+    
+
+    
+
+    // const getOptions = paymentSelector.children
+    // console.log(getOptions)
+
+
+})
+
+
+
+
 
 
 
